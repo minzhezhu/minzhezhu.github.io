a7039ab2d1a0781eb44dc3e5dbbd1991494d48bc
minzhezhu <zhumz20@fudan.edu.cn>
2026-07-26 00:53:00 +0000

    Deploying to gh-pages from @ minzhezhu/minzhezhu.github.io@d4dbafc4d8dfa9da5496bb8d5d72e12e2bb40c0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,10 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{
+    },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
